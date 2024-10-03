945e93fe11ece6d64578760c39ee0e794804d4b4
887 <2300887@gmail.com>
2024-10-03 22:26:50 +0200

    send + sync

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
+#[cfg(all(feature = "log", not(feature = "log-file")))]
 use tracing_subscriber::filter;
 
 //not - can't use     tracing_subscriber::fmt::init();
 //anything that pins itself to memory is incompatible with hot-reload and defeats the purpose
 
-pub type Result<T> = core::result::Result<T, Box<dyn std::error::Error + Send>>;
+pub type Result<T> = core::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
 #[cfg(all(feature = "log", not(feature = "log-file")))]
 pub type LogGuard = tracing::subscriber::DefaultGuard;
