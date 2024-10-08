754780baecaa7ae6ecca4ca8ddc3a86d7e8cd0d7
887 <2300887@gmail.com>
2024-10-09 01:45:20 +0200

    hide logging specifics

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-#[cfg(all(feature = "log", not(feature = "log-file")))]
 use tracing_subscriber::filter;
 
 //not - can't use     tracing_subscriber::fmt::init();
@@ -7,40 +6,83 @@ use tracing_subscriber::filter;
 pub type Result<T> = core::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
 #[cfg(all(feature = "log", not(feature = "log-file")))]
-pub type LogGuard = tracing::subscriber::DefaultGuard;
+pub struct LogGuard {
+    subscriber_guard: tracing::subscriber::DefaultGuard,
+}
 
 #[cfg(all(feature = "log", feature = "log-file"))]
-pub type LogGuard = (
-    tracing::subscriber::DefaultGuard,
-    tracing_appender::non_blocking::WorkerGuard,
-);
+pub struct LogGuard {
+    subscriber_guard: tracing::subscriber::DefaultGuard,
+    file_flush_guard: tracing_appender::non_blocking::WorkerGuard,
+}
 
 #[cfg(all(feature = "log", not(feature = "log-file")))]
-pub fn get_subscription() -> Result<tracing::subscriber::DefaultGuard> {
+pub struct LogSubscriber {
+    log_subscriber: tracing_subscriber::FmtSubscriber<
+        tracing_subscriber::fmt::format::DefaultFields,
+        tracing_subscriber::fmt::format::Format,
+        filter::EnvFilter,
+    >,
+}
+
+#[cfg(all(feature = "log", not(feature = "log-file")))]
+pub fn get_subscription() -> Result<LogGuard> {
+    let log_subscriber = get_subscriber()?;
+    Ok(get_subscription_for_subscriber(log_subscriber))
+}
+
+#[cfg(all(feature = "log", not(feature = "log-file")))]
+pub fn get_subscription_for_subscriber(log_subscriber: LogSubscriber) -> LogGuard {
+    let subscriber_guard = tracing::subscriber::set_default(log_subscriber.log_subscriber);
+    LogGuard { subscriber_guard }
+}
+
+#[cfg(all(feature = "log", not(feature = "log-file")))]
+fn get_subscriber() -> Result<LogSubscriber> {
     let log_subscriber = tracing_subscriber::fmt()
         // filter spans/events with level TRACE or higher.
         // .with_max_level(Level::TRACE)
         .with_env_filter(filter::EnvFilter::from_default_env())
         // build but do not install the subscriber.
         .finish();
+    Ok(LogSubscriber { log_subscriber })
+}
 
-    Ok(tracing::subscriber::set_default(log_subscriber))
+#[cfg(all(feature = "log", feature = "log-file"))]
+pub struct LogSubscriber {
+    file_flush_guard: tracing_appender::non_blocking::WorkerGuard,
+    log_subscriber: tracing_subscriber::FmtSubscriber<
+        tracing_subscriber::fmt::format::DefaultFields,
+        tracing_subscriber::fmt::format::Format,
+        tracing_subscriber::EnvFilter,
+        tracing_appender::non_blocking::NonBlocking,
+    >,
 }
 
 #[cfg(all(feature = "log", feature = "log-file"))]
 pub fn get_subscription() -> Result<LogGuard> {
+    let log_subscriber = get_subscriber()?;
+    Ok(get_subscription_for_subscriber(log_subscriber))
+}
+
+#[cfg(all(feature = "log", feature = "log-file"))]
+pub fn get_subscription_for_subscriber(log_subscriber: LogSubscriber) -> LogGuard {
+    let subscriber_guard = tracing::subscriber::set_default(log_subscriber.log_subscriber);
+    let file_flush_guard = log_subscriber.file_flush_guard;
+
+    LogGuard {
+        subscriber_guard,
+        file_flush_guard,
+    }
+}
+
+#[cfg(all(feature = "log", feature = "log-file"))]
+fn get_subscriber() -> Result<LogSubscriber> {
     let log_dir = std::env::var("LOG_PATH").map_err(|e| format!("LOG_PATH is not set {:?}", e))?;
     let log_prefix =
         std::env::var("LOG_PREFIX").map_err(|e| format!("LOG_PREFIX is not set {:?}", e))?;
-
-    //https://docs.rs/tracing-appender/latest/tracing_appender/
     let file_appender = tracing_appender::rolling::daily(log_dir, log_prefix);
     let (non_blocking, file_flush_guard) = tracing_appender::non_blocking(file_appender);
-
-    //https://stackoverflow.com/questions/73225598/why-is-nothing-printed-to-the-terminal-when-using-tracing-appender
-    //tracing-appender only logs to the file when enabled, to also log to standard output you need to add that as well
-    //let (non_blocking, guard) = tracing_appender::non_blocking(std::io::stdout());
-
     let log_subscriber = tracing_subscriber::fmt()
         // filter spans/events with level TRACE or higher.
         // .with_max_level(Level::TRACE)
@@ -48,7 +90,8 @@ pub fn get_subscription() -> Result<LogGuard> {
         .with_writer(non_blocking)
         // build but do not install the subscriber.
         .finish();
-
-    let subscriber_guard = tracing::subscriber::set_default(log_subscriber);
-    Ok((subscriber_guard, file_flush_guard))
+    Ok(LogSubscriber {
+        file_flush_guard,
+        log_subscriber,
+    })
 }
